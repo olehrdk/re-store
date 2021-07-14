4305e48abd2e9232c31a713781e6760e312c8d3a
olehrdk <olehrudak981@gmail.com>
2021-07-14 20:54:36 +0300

    Changes in redirect. App deployed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 
 import App from './components/app'
 import ErrorBoundry from './components/error-boundry'
@@ -14,7 +14,7 @@ ReactDOM.render(
    <Provider store={store}>
       <ErrorBoundry>
          <BookstoreServiceProvider value={bookstoreService}>
-            <Router>
+            <Router basename={process.env.PUBLIC_URL}>
                <App />
             </Router>
          </BookstoreServiceProvider>         
